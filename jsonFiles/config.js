3aaf39739f7984380289976ab9409ce55ba13e0d
Raxieuwu <67073445+Raxieuwu@users.noreply.github.com>
2020-08-27 20:10:07 +0200

    Create config.js

---
+++ jsonFiles/config.js
@@ -0,0 +1,9 @@
+{
+  "name": "Zetra bot",
+  "version": "1.0.0",
+  "description": "ur mom is gay",
+  "main": "index.js",
+  "token": "NTc0MzMzNjMxNzY2NTkzNTM3.XM334A.QoOsQeFlBw4sml6_EujqNzXKPJY",
+  "author": "Raxie#8877",
+  "prefix": "."
+}
